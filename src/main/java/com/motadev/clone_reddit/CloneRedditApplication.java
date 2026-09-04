d50f8723101a8955f0a5f0f5d85cdc2c7460e635
Filipemt <contato.motadev@gmail.com>
2026-09-03 21:28:44 -0300

    initial commit

---
+++ src/main/java/com/motadev/clone_reddit/CloneRedditApplication.java
@@ -0,0 +1,13 @@
+package com.motadev.clone_reddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneRedditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motadev/clone_reddit/CloneRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motadev.clone_reddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneRedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
