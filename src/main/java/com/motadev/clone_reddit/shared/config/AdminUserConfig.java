c9ba381120261e702c37bdbb0b4ff0ed6544040c
Filipe Mota <contato.motadev@gmail.com>
2026-09-09 19:00:57 -0300

    feat(auth): seed default admin user on startup

---
+++ src/main/java/com/motadev/clone_reddit/shared/config/AdminUserConfig.java
@@ -0,0 +1,56 @@
+package com.motadev.clone_reddit.shared.config;
+
+import com.motadev.clone_reddit.auth.entity.Role;
+import com.motadev.clone_reddit.auth.entity.User;
+import com.motadev.clone_reddit.auth.entity.enums.RoleValues;
+import com.motadev.clone_reddit.auth.repository.RoleRepository;
+import com.motadev.clone_reddit.auth.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
+
+@Configuration
+@Slf4j
+public class AdminUserConfig implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AdminUserConfig(UserRepository userRepository,
+                           RoleRepository roleRepository,
+                           PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+        var roleAdmin = roleRepository.findByName(RoleValues.ADMIN.name())
+                .orElseGet(() -> {
+                    var newRole = new Role();
+                    newRole.setName(RoleValues.ADMIN.name());
+                    return roleRepository.save(newRole);
+                });
+
+        var userAdmin = userRepository.findByUsername("admin");
+
+        userAdmin.ifPresentOrElse(
+                user -> log.info("User already exists."),
+                () -> {
+                    var user = new User();
+                    user.setUsername("admin");
+                    user.setPassword(passwordEncoder.encode("123"));
+                    user.setRoles(Set.of(roleAdmin));
+                    userRepository.save(user);
+                    log.info("Admin user created successfully.");
+                }
+        );
+    }
+}
