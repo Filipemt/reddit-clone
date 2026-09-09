ea0d8078b9fe18a4b0217bd39b4494ad258a7484
Filipe Mota <contato.motadev@gmail.com>
2026-09-09 19:16:54 -0300

    feat(auth): add role scope claim to jwt token

--- src/main/java/com/motadev/clone_reddit/auth/service/impl/TokenServiceImpl.java
+++ src/main/java/com/motadev/clone_reddit/auth/service/impl/TokenServiceImpl.java
@@ -1,6 +1,7 @@
 package com.motadev.clone_reddit.auth.service.impl;
 
 import com.motadev.clone_reddit.auth.dtos.response.TokenData;
+import com.motadev.clone_reddit.auth.entity.Role;
 import com.motadev.clone_reddit.auth.entity.User;
 import com.motadev.clone_reddit.auth.service.TokenServiceI;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +11,7 @@ import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.stream.Collectors;
 
 @Service
 public class TokenServiceImpl implements TokenServiceI {
@@ -25,10 +27,17 @@ public class TokenServiceImpl implements TokenServiceI {
     @Override
     public TokenData generateToken(User user) {
         var now = Instant.now();
+
+        var scope = user.getRoles()
+                .stream()
+                .map(Role::getName)
+                .collect(Collectors.joining(" "));
+
         var claims = JwtClaimsSet.builder()
                 .issuer("backend-reddit-clone")
                 .subject(user.getUserId().toString())
                 .issuedAt(now)
+                .claim("scope", scope)
                 .expiresAt(now.plusSeconds(this.expiresIn))
                 .build();
 
