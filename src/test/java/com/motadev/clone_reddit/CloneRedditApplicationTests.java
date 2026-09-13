716e881ebb1beea293726c527987e07d724a7e5b
Filipe Mota <contato.motadev@gmail.com>
2026-09-13 16:24:45 -0300

    test(shared): run context smoke test with testcontainers

--- src/test/java/com/motadev/clone_reddit/CloneRedditApplicationTests.java
+++ src/test/java/com/motadev/clone_reddit/CloneRedditApplicationTests.java
@@ -1,9 +1,14 @@
 package com.motadev.clone_reddit;
 
+import com.motadev.clone_reddit.support.TestcontainersConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
 class CloneRedditApplicationTests {
 
 	@Test
